123b17b68e0fa9af6945bba5a64a649bcc472b98
Juan Felipe <juanalvear2009@gmail.com>
2023-04-12 22:51:50 -0500

    Cambios de seguridad

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Example
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Habilitar la protección CSRF
+    config.action_controller.default_protect_from_forgery = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
