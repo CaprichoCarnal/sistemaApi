df3452f97f42e219bc743da3e6ef1cf2be67d38a
Capricho <carnalcapricho@gmail.com>
2023-04-19 02:16:56 +0000

    Contenido en la nube

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+   # skip_before_action :verify_authenticity_token
     before_action :verify_authenticity_token
-
+    skip_before_action :verify_authenticity_token
     include JsonWebToken
     before_action :authenticate_request
 
--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,15 @@ module Example
     config.load_defaults 7.0
 
     # Habilitar la protección CSRF
-    config.action_controller.default_protect_from_forgery = false
-
+    config.action_controller.default_protect_from_forgery = true
+    
+     # Configuración de CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # Permitir solicitudes desde cualquier origen. Cambia '*' por los orígenes permitidos según tus necesidades.
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
