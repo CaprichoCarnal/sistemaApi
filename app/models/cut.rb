089419169232b13253f9bf4e6a6ffe7f8bdfd1b3
Juan Felipe <juanalvear2009@gmail.com>
2023-06-10 10:28:05 -0500

    actualizar materias primas disponobles

--- app/models/cut.rb
+++ app/models/cut.rb
@@ -3,6 +3,7 @@ class Cut < ApplicationRecord
 
   after_save :update_inventory
   after_update :check_availability
+  after_create :change_raw_material_availability
 
   has_many :inventories, as: :item
 
@@ -27,10 +28,15 @@ class Cut < ApplicationRecord
   def check_availability
     if available_for_sale_changed? && !available_for_sale
       remove_from_inventory
+      
     end
   end
 
   def remove_from_inventory
     Inventory.where(item: self).destroy_all
+  end
+
+  def change_raw_material_availability
+    raw_material.update(available: 'No')
   end
 end
